28a847314dbaa946e5ba8fa2e2975f915ab6d3e2
tvpian <tharunv.pvit@gmail.com>
2025-08-17 18:06:55 -0400

    chore: open source initial release

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [{
+    name: "ultranote",
+    script: "server.js",
+    env: { NODE_ENV: "production", PORT: 3366 },
+    watch: true,
+    instances: 1, // or "max" for cluster
+    autorestart: true
+  }]
+}
