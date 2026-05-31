c9bf98653762433bda14add09d05a0748e46bb96
tvpian <tharunv.pvit@gmail.com>
2026-05-31 15:02:41 -0400

    style: force visible red border on .katex-error block (CSS class, not inline)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ultranote-lite-v76-wiki-sentinel';
+const CACHE = 'ultranote-lite-v77-error-css';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/styles.css', '/app.js', '/autosync.js', '/ui-polish.js', '/ui-extras.js', '/editor-extras.js', '/power-features.js', '/research-mode.js', '/fonts/fonts.css'];
 self.addEventListener('install', e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
